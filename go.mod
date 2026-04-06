0f17ca8f17d336837cc2b8ed57264d1d0cd8232e
Giulio Frasca <gfrasca@redhat.com>
2026-04-06 18:26:18 -0400

    feat(server): Add basic http server
    Signed-off-by: Giulio Frasca <gfrasca@redhat.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gfrasca/rhobs-challenge
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("PUT /objects/{bucket}/{objectID}", handlePut)
+	mux.HandleFunc("GET /objects/{bucket}/{objectID}", handleGet)
+	mux.HandleFunc("DELETE /objects/{bucket}/{objectID}", handleDelete)
+
+	addr := fmt.Sprintf(":%s", port)
+	log.Printf("starting server on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request) {
+	objectID := r.PathValue("objectID")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"id": objectID})
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not found", http.StatusNotFound)
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not found", http.StatusNotFound)
+}
